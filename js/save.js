dddafc909b27e0946c3138a857b6431f10f19430
Alfonso M. Evangelista <a46.evangelista@gmail.com>
2019-05-08 08:33:14 -0400

    sas

--- js/save.js
+++ js/save.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-   $('#frm-save').submit(function(event){
+$(document).ready(function () {
+    $('#frm-save').submit(function (event) {
 
         // Obtener el nombre del archivo.
         var filename = $('#txtfilename').val();
@@ -10,16 +10,16 @@ $(document).ready(function() {
         // Obtener los datos.
         var textData = $('#textData').val();
 
-      
-      if(filename != '' && format != '' && textData != ''){
-        var blob = new Blob([textData], {type:'text/plain;charset=utf-8'});
-        saveAs(blob, filename +'.'+ format);
-    }
-       event.preventDefault();
-   });
+        if (filename != '' && format != '' && textData != '') {
+            var blob = new Blob([textData], { type: 'text/plain;charset=utf-8' });
+            saveAs(blob, filename + '.' + format);
+        }
 
-	$('#btn-getdata').click(function() {
-		 try {
+        event.preventDefault();
+    });
+
+    $('#btnjsondata').click(function () {
+        try {
             $.ajax({
                 url: "http://dummy.restapiexample.com/api/v1/employees",
                 type: "GET",
@@ -28,18 +28,11 @@ $(document).ready(function() {
                     alert("Error obteniendo los datos: " + xhr + ", " + text + ", status=" + status);
                 },
                 success: function (result) {
-                	
-                	console.log(JSON.stringify(result));
-                    // var sel = $("#idCiudad");
-                    // sel.empty();
-
-                    // $.each(result, function (i, item) {
-                    //     $(sel).append('<option value="' + item.IdCiudad + '">' + item.Nombre + '</option>');
-                    // });
+                    alert(JSON.stringify(result));
                 }
             });
         } catch (e) {
             alert("Error: " + e.toString());
-        }		
-	});;
+        }
+    });
 });
