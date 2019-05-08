67e9c86f0713b4af8309510e58c648908f0f4e1e
Alfonso M. Evangelista <a46.evangelista@gmail.com>
2019-05-07 22:46:16 -0400

    Initial Commit
    Se suben todos los archivos del proyecto.

---
+++ js/save.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+   $('#frm-save').submit(function(event){
+
+        // Obtener el nombre del archivo.
+        var filename = $('#txtfilename').val();
+
+        // Obtener el formato del archivo.
+        var format = $('#cbxformat').children("option:selected").val();
+      
+      if(filename != '' && format != ''){
+        var blob = new Blob(['Alfonso Miguel Evangelista Concepción'], {type:'text/plain;charset=utf-8'});
+        saveAs(blob, filename +'.'+ format);
+    }
+       event.preventDefault();
+   });
+
+	$('#btn-getdata').click(function() {
+		 try {
+            $.ajax({
+                url: "http://dummy.restapiexample.com/api/v1/employees",
+                type: "GET",
+                dataType: 'json',
+                error: function (xhr, text, status) {
+                    alert("Error obteniendo los datos: " + xhr + ", " + text + ", status=" + status);
+                },
+                success: function (result) {
+                	
+                	console.log(JSON.stringify(result));
+                    // var sel = $("#idCiudad");
+                    // sel.empty();
+
+                    // $.each(result, function (i, item) {
+                    //     $(sel).append('<option value="' + item.IdCiudad + '">' + item.Nombre + '</option>');
+                    // });
+                }
+            });
+        } catch (e) {
+            alert("Error: " + e.toString());
+        }		
+	});;
+});
